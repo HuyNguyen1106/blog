8dbbb5786c121d272ab5e415b13ee62a6512ac70
huynguyen <huynguyen@bravesoft.com.vn>
2021-02-18 10:46:46 +0700

    sort middleware

--- src/app/controllers/MyController.js
+++ src/app/controllers/MyController.js
@@ -5,15 +5,23 @@ const { mapObject, mapArray } = require('../../util/mongoose');
 class MyController {
 
     // GET /me/stored/courses
-    myCourses(req, res, next) {
-        Promise.all([Course.find().populate('category'), Course.countDocumentsDeleted()])
-            .then(([courses, deletedCount]) => {
-                res.render('me/myCourses', {
-                    courses: mapArray(courses),
-                    deletedCount
-                })
-            })
-            .catch(next)
+	myCourses(req, res, next) {
+		let query = Course.find().populate("category");
+
+		if (req.query.hasOwnProperty('_sort')) {
+			query = query.sort({
+				[req.query.column]: req.query.type
+			})
+		}
+
+        Promise.all([query, Course.countDocumentsDeleted()])
+			.then(([courses, deletedCount]) => {
+				res.render("me/myCourses", {
+					courses: mapArray(courses),
+					deletedCount,
+				});
+			})
+			.catch(next);
     }
 
     // GET /me/trash/courses
--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-const path = require('path');
-const express = require('express');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const path = require("path");
+const express = require("express");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
 const app = express();
 
-const db = require('./config/db');
+const SortMiddleware = require("./app/middlewares/SortMiddleware");
+const db = require("./config/db");
 
 // DB Connect
 db.connect();
 
-const route = require('./routes/index');
-const Category = require('./app/models/Category');
+const route = require("./routes/index");
+const Category = require("./app/models/Category");
 
 // override with POST having ?_method=DELETE/PUT
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 
 // HTTP log
 // app.use(morgan('combined'));
 // Static
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
 	express.urlencoded({
@@ -27,19 +28,39 @@ app.use(
 	})
 );
 app.use(express.json());
+app.use(SortMiddleware);
 
 app.engine(
-	'hbs',
+	"hbs",
 	handlebars({
-		extname: '.hbs',
+		extname: ".hbs",
 		helpers: {
 			sum: (a, b) => a + b,
 			equals: (a, b) => a === b,
-		}
+			sortable: (field, sort) => {
+				const sortType = field === sort.column ? sort.type : "default";
+
+				const icons = {
+					default: "fas fa-sort",
+					asc: "fas fa-sort-up",
+					desc: "fas fa-sort-down",
+				};
+				const types = {
+					default: "desc",
+					desc: "asc",
+					asc: "desc",
+				};
+
+				const icon = icons[sortType];
+				const type = types[sortType];
+
+				return `<a href="?_sort&column=${field}&type=${type}"><i class="${icon}"></i></a>`;
+			},
+		},
 	})
 );
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources', 'views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
 
 const port = 3000;
 
